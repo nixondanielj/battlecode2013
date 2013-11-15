f092dc8de58ed086a6e93d88f682024dd3db29cb
dan <nixon.daniel.j@gmail.com>
2013-11-14 19:27:17 -0500

    added strategies

--- teams/team912/robots/strategies/IStrategy.java
+++ teams/team912/robots/strategies/IStrategy.java
@@ -7,5 +7,13 @@ import battlecode.common.RobotController;
 public interface IStrategy {
 	MapLocation getTarget(Mapper mapper);
 	
-	int[][] getMessage(RobotController rc);
+	int[] getMessage(RobotController rc);
+	
+	void doPreMoveAction(RobotController rc);
+	
+	void doPostMoveAction(RobotController rc);
+	
+	void doNoMoveAction(RobotController rc);
+	
+	void doInactiveAction(RobotController rc);
 }
