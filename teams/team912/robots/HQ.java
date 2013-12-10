bf465e5a3661232fa6b63e6f5bf343155b95823c
Chris Fowers <christopher.fowers@onshoreoutsourcing.com>
2013-12-10 11:21:44 -0500

    Updated team912.robots.HQ for improved spawning location

--- teams/team912/robots/HQ.java
+++ teams/team912/robots/HQ.java
@@ -1,10 +1,13 @@
 package team912.robots;
 
+//import team912.mapping.Mapper;
+//import team912.pathing.BadPather;
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
-import battlecode.common.GameObject;
+//import battlecode.common.GameObject;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
+import battlecode.common.Team;
 
 public class HQ extends BaseRobot {
 	public HQ(RobotController rc) {
@@ -16,16 +19,43 @@ public class HQ extends BaseRobot {
 	public void run() throws GameActionException {
 		if (this.getControl().isActive()) {
 			Direction direction;
-			GameObject obstacle;
-			do {
-				direction = Direction.values()[(int) (Math.random() * Direction
-						.values().length)];
-				MapLocation location = this.getControl().getLocation()
-						.add(direction);
-				obstacle = this.getControl().senseObjectAtLocation(location);
-			} while (obstacle != null);
+			direction = getBestSpawnDirection();
 			this.getControl().spawn(direction);
 		}
 	}
-
+	
+	private Direction getBestSpawnDirection() throws GameActionException{
+		RobotController c = this.getControl();
+		MapLocation hqLoc = c.getLocation();
+		MapLocation enLoc = c.senseEnemyHQLocation();
+		Direction enemyHqDir = hqLoc.directionTo(enLoc);
+		
+		if(isSafeLocation(hqLoc.add(enemyHqDir), c)){
+			return enemyHqDir;
+		} else if (isSafeLocation(hqLoc.add(enemyHqDir.rotateLeft()), c)){
+			return enemyHqDir.rotateLeft();
+		} else if (isSafeLocation(hqLoc.add(enemyHqDir.rotateRight()), c)){
+			return enemyHqDir.rotateRight();
+		} else if (isSafeLocation(hqLoc.add(enemyHqDir.rotateLeft().rotateLeft()), c)){
+			return enemyHqDir.rotateLeft().rotateLeft();
+		} else if (isSafeLocation(hqLoc.add(enemyHqDir.rotateRight().rotateRight()), c)){
+			return enemyHqDir.rotateRight().rotateRight();
+		} else if (isSafeLocation(hqLoc.add(enemyHqDir.rotateLeft().rotateLeft().rotateLeft()), c)){
+			return enemyHqDir.rotateLeft().rotateLeft().rotateLeft();
+		} else if (isSafeLocation(hqLoc.add(enemyHqDir.rotateRight().rotateRight().rotateRight()), c)){
+			return enemyHqDir.rotateRight().rotateRight().rotateRight();
+		} else if (isSafeLocation(hqLoc.add(enemyHqDir.opposite()), c)){
+			return enemyHqDir.opposite();
+		}
+		return enemyHqDir;
+	}
+	
+	private boolean isSafeLocation(MapLocation location, RobotController c) throws GameActionException{		
+		Team mine = c.senseMine(location);
+				
+		if (mine == c.getTeam() || mine == null){
+			return true;
+		}
+		return false;
+	}
 }
